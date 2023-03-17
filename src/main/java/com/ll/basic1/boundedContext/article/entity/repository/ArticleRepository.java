c88387a0bbf42e6f5c065b8487f8bb06e448653a
havegrit <shin_jaean@naver.com>
2023-03-17 11:43:01 +0900

    modify ArticleRepository

--- src/main/java/com/ll/basic1/boundedContext/article/entity/repository/ArticleRepository.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/repository/ArticleRepository.java
@@ -3,5 +3,5 @@ package com.ll.basic1.boundedContext.article.entity.repository;
 import com.ll.basic1.boundedContext.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
